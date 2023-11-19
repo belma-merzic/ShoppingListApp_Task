797f77957dbd7e24883d7ef27f3e320cb978c2c3
belma-merzic <naida@fished.com>
2023-11-19 23:06:22 +0100

    fixed

--- SQL script.sql
+++ SQL script.sql
@@ -1,32 +1,6 @@
-create database Task_DB
-
-go
-
 use Task_DB
 
 go
-
-create table Shopper
-(
- ShopperID int primary key identity(1,1),
- ShopperName nvarchar(50)
-)
-
-create table Item
-(
- ItemID int primary key identity(1,1),
- ItemName nvarchar(50)
-)
-
-create table ShoppingList
-(
- ShoppingListID int primary key identity(1,1),
- ShopperID int,
- ItemID int,
-
- FOREIGN KEY (ShopperID) REFERENCES Shopper(ShopperID),
- FOREIGN KEY (ItemID) REFERENCES Item(ItemID),
-)
 
 insert into Shopper (ShopperName) values 
 ('Shopper1'),
